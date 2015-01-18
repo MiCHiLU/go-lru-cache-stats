f4da404c089f0e7051846361d431eb089e717fef
ENDOH takanao <djmchl@gmail.com>
2015-01-18 10:06:27 +0700

    separate Context

--- context.go
+++ context.go
@@ -18,15 +18,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// peers.go defines how processes find and communicate with their peers.
-
 package lru
 
-import (
-	pb "github.com/golang/groupcache/groupcachepb"
-)
-
-// ProtoGetter is the interface that must be implemented by a peer.
-type ProtoGetter interface {
-	Get(context Context, in *pb.GetRequest, out *pb.GetResponse) error
-}
+// Context is an opaque value passed through calls to the
+// ProtoGetter. It may be nil if your ProtoGetter implementation does
+// not require a context.
+type Context interface{}
