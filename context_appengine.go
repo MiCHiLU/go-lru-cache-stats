bc7f2f888936e9fe3410839e4008d973b5b7264b
ENDOH takanao <djmchl@gmail.com>
2015-01-18 10:11:55 +0700

    copy context.go to context_appengine.go

---
+++ context_appengine.go
@@ -0,0 +1,26 @@
+/*
+Copyright 2015 ENDOH takanao.
+<https://github.com/MiCHiLU/go-lru-cache-stats>
+
+Copyright 2012 Google Inc.
+<https://github.com/golang/groupcache>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package lru
+
+// Context is an opaque value passed through calls to the
+// ProtoGetter. It may be nil if your ProtoGetter implementation does
+// not require a context.
+type Context interface{}
